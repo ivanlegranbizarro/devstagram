d0655718c79c8f17a97148c9d8131b55e5b1c1af
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-03-10 10:27:07 +0100

    create post component with livewire

---
+++ app/Http/Livewire/PostsComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class PostsComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.posts-component');
+    }
+}
