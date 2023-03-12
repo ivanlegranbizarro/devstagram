9b18e2153314b0b8502b6e7475b2882bd92f938a
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-03-12 09:03:55 +0100

    posts con la gente a la que seguimos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function __invoke()
     {
-        return view('home');
+        if (auth()->user()) {
+            $ids = auth()->user()->following->pluck('id')->toArray();
+            $posts = Post::whereIn('user_id', $ids)->latest()->paginate(5);
+            return view('home', [
+                'posts' => $posts
+            ]);
+        } else {
+            return redirect()->route('login');
+        }
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,5 +5,23 @@ Página principal
 @endsection
 
 @section('contenido')
-<h1>Contenido de la página principal</h1>
+@if ($posts->count())
+<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+  @foreach ($posts as $post)
+  <a href="{{ route('posts.show', ['user'=>$post->user, 'post' =>$post]) }}">
+    <div class="bg-white shadow-sm rounded-md p-4">
+      <h3 class="text-sm text-gray-500">{{ $post->user->name }}</h3>
+      <h2 class="text-lg font-medium text-gray-800">{{ $post->titulo }}</h2>
+      <p class="text-sm text-gray-500 mt-2">{{ $post->descripcion }}</p>
+      <img src="{{ asset('uploads/' . $post->imagen) }}" alt="{{ $post->titulo }}" class="mt-4">
+    </div>
+  </a>
+  @endforeach
+</div>
+<div class="my-10 flex justify-center">
+  <div class="livewire-pagination">{{$posts->links()}}</div>
+</div>
+@else
+<p>Todavía no hay publicaciones de la gente a la que sigues</p>
+@endif
 @endsection
